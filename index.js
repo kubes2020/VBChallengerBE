86dedd0081630c82e75ccc602c5ece111b06aacd
Brian Kubes <briankubes@gmail.com>
2021-05-28 17:10:52 -0700

    sockit.io is starting to communicate with FE

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ require("dotenv").config();
 const server = require("./api/server");
 
 const PORT = process.env.PORT || 4220;
+// server.listen(PORT, () => {
+//     console.log(`\n listening on port ${PORT}`);
+// });
 
-server.listen(PORT, () => {
-    console.log(`\n listening on port ${PORT}`);
+// specific to socket.io
+const httpServer = require("http").createServer(server);
+const io = require("socket.io")(httpServer, { cors: { origin: "*" } });
+httpServer.listen(PORT);
+
+io.on("connection", (socket) => {
+    console.log("Socket connected to BE with id:", socket.id);
+    socket.emit("connection", "Yes we are up and running");
+    socket.on("message", (data) => {
+        // this will send message to everyone except myself
+        socket.broadcast.emit("message", data);
+        console.log("this is data received by BE:", data);
+    });
 });
